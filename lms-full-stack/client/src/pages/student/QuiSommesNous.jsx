d7fbafe716d8af4c0f73c07bed8cf7635e1cd7e3
moussagouasmia <151625586+moussagouasmia@users.noreply.github.com>
2025-04-19 10:38:30 +0100

    Create QuiSommesNous.jsx

---
+++ lms-full-stack/client/src/pages/student/QuiSommesNous.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function About() {
+  return (
+    <div>
+      <h1>Qui sommes-nous</h1>
+      <p>محتوى صفحة "من نحن" هنا.</p>
+    </div>
+  );
+}
+
+export default About;
