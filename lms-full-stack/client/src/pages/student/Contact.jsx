634803cd7c58398a3f853f18fefd0b13077fdd1d
moussagouasmia <151625586+moussagouasmia@users.noreply.github.com>
2025-04-19 10:40:12 +0100

    Create Contact.jsx

---
+++ lms-full-stack/client/src/pages/student/Contact.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Contact() {
+  return (
+    <div>
+      <h1>Pour nous contacter</h1>
+      <p>محتوى صفحة "اتصل بنا" هنا.</p>
+    </div>
+  );
+}
+
+export default Contact;
